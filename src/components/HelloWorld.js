f7027de9e9b8c0e1602cf28dbe87c7d03ce387e0
Karthik M A M <karthik_m_a_m@outlook.com>
2017-02-28 10:54:50 +0530

    added a simple synthetic event to the HelloWorld component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -9,11 +9,17 @@ export class HelloWorld extends React.Component {
         };
     }
 
+    onClickHandler(e) {
+        this.setState((prevState, props) => ({
+            'greeting': prevState.greeting == 'Hello' ? 'Bye' : 'Hello'
+        }));
+    }
+
     static propTypes = {
         name: React.PropTypes.string.isRequired
     };
 
     render() {
-        return <h1> { this.state.greeting } { this.props.name } </h1>;
+        return <h1 onClick={ this.onClickHandler.bind(this) }> { this.state.greeting } { this.props.name } </h1>;
     }
 }
