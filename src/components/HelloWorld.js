0e1a72a357da512ac1fdbab68c9bebf98524c45b
Karthik M A M <karthik_m_a_m@outlook.com>
2017-02-28 10:48:22 +0530

    added a simple state to the HelloWorld component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            greeting: 'Hello'
+        };
+    }
 
     static propTypes = {
         name: React.PropTypes.string.isRequired
     };
 
     render() {
-        return <h1> Hello { this.props.name } </h1>;
+        return <h1> { this.state.greeting } { this.props.name } </h1>;
     }
 }
